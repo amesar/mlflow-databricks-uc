39f2f14619585782a4f1a639d114099482ad9d04
amesar <amesar>
2023-07-17 04:54:05 +0000

    _README.py: notebook link fix

--- notebooks/_README.py
+++ notebooks/_README.py
@@ -11,7 +11,7 @@
 # MAGIC * [Register_Model]($Register_Model) - Register a new model - create new model version.
 # MAGIC * [List_Registered_Models]($List_Registered_Models) - List registered models.
 # MAGIC * [List_Models_Versions]($List_Models_Versions) - List model versions of a registered model.
-# MAGIC * [Get_Models_Version]($Get_Models_Version) - Get model version, its run and MLflow model.
+# MAGIC * [Get_Model_Version]($Get_Model_Version) - Get model version, its run and MLflow model.
 # MAGIC * [Common]($Common) - Common utilities.
 # MAGIC
 # MAGIC Last updated: 2023-07-16
